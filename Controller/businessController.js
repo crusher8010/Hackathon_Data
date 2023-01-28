34ad4230439e3d66b7e0254a074a32599d5b513d
crusher8010 <crusher8010@gmail.com>
2023-01-28 12:58:08 +0530

    slight change

--- Controller/businessController.js
+++ Controller/businessController.js
@@ -3,11 +3,11 @@ const businessData = require('../models/businessModel');
 exports.getAllbusinessData = async (req, res) => {
     try {
         let businessdata = await businessData.find();
-
+        console.log(businessData)
         res.status(200).json({
             status: "success",
             results: businessdata.length,
-            businessData
+            businessdata
         })
     } catch (err) {
         res.status(400).json({
