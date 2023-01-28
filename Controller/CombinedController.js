5ab46e0c840f652bdf1910b440ce77bbad49229d
crusher8010 <crusher8010@gmail.com>
2023-01-28 22:36:26 +0530

    added filter api

--- Controller/CombinedController.js
+++ Controller/CombinedController.js
@@ -16,3 +16,20 @@ exports.getAllCombinedData = async (req, res) => {
         })
     }
 }
+
+exports.getFilteredData = async (req, res) => {
+    try {
+        let combineddata = await CombinedData.find(req.query);
+
+        res.status(200).json({
+            status: "success",
+            results: combineddata.length,
+            combineddata
+        })
+    } catch (err) {
+        res.status(400).json({
+            status: "fail",
+            err
+        })
+    }
+}
--- routes/CombinedRoutes.js
+++ routes/CombinedRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const combinedController = require('../Controller/CombinedController');
 
-const { getAllCombinedData } = combinedController;
+const { getAllCombinedData, getFilteredData } = combinedController;
 
 const Router = express.Router();
 
 Router.route('/').get(getAllCombinedData);
+Router.route('/filter').get(getFilteredData);
 
 module.exports = Router;
