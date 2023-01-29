4b78434593a67e5151c3662bd639a3a7c836a81f
crusher8010 <crusher8010@gmail.com>
2023-01-29 10:59:51 +0530

    added socket function

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ dotenv.config({ path: './config.env' });
 
 const app = express();
 app.use(express.json());
+const http = require('http').Server(app)
 
-app.use(cors({
-  origin: "*"
-}));
+const socketIO = require('socket.io')(http, {
+  cors: {
+    origin: "*"
+  }
+})
 
 app.use('/', userRoutes);
 app.use('/ai', aiRoutes);
@@ -30,10 +33,29 @@ app.use('/combined', combinedRoutes)
 
 mongoose.set("strictQuery", false);
 
+socketIO.on('connection', (socket) => {
+  console.log('connected');
+  socket.on('message', (data) => {
+    socket.broadcast.emit('message', data);
+  });
+
+  socket.on('newUser', (data) => {
+    //Adds the new user to the list of users
+    users.push(data);
+    // console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit('newUserResponse', users);
+  });
+
+  // socket.on('disconnect', () => {
+  //     console.log('🔥: A user disconnected');
+  // });
+});
+
 const DB = process.env.URL.replace('<PASSWORD>', process.env.PASSWORD);
 mongoose.connect(DB).then(() => console.log('Database Connection formed..')).catch(err => console.log('No Database Connection...'))
 
 const port = process.env.PORT;
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Database is running on ${port}`);
 })
